47d6c163c547232c8b3bb46b941813b8ad86c9bd
Ubuntu <ubuntu@ip-172-31-34-19.eu-west-2.compute.internal>
2020-10-10 15:48:22 +0000

    settings: changed Secret key location to be in a local file and add MEDIA root. urls: additional imports and prepping for later on with MEDIA_URL and MEDIA_ROOT for images.

--- sweetstuff/settings.py
+++ sweetstuff/settings.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 import os
 from pathlib import Path
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f10(0+@8m(&9rza50*t8#c+9ir4(-hnu7x&-)t^dc7f(8ujj8v'
-
+try:
+    with open('./keys.txt') as f:
+        SECRET_KEY = f.read().strip()
+except:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
