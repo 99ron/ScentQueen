22cb39f83e792c239b8e6d22c75d991d466d9f4c
Ubuntu <ubuntu@ip-172-31-34-19.eu-west-2.compute.internal>
2020-10-25 16:35:21 +0000

    Settings: Removed static_root and added Staticfiles_dirs location as stylesheets weren't being found. urls.py: Correct static URL path. base.html: Complete overhaul using an ecommerce template for me to work from. Static: additional stylesheet for the base.html file to function properly.

--- sweetstuff/settings.py
+++ sweetstuff/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'sweetstuff.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,4 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = "/home/ubuntu/environment/static"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
