d716e6a4d1b773ffc4715ff1e4f02b5579a3ee2a
Aaron <azza_5252@hotmail.com>
2020-11-29 16:51:39 +0000

    Restructured my sass files and have seperated the logic for the main layout and created another for animations. Style.scss is importing both of these and placing it into one CSS file as it did before. Functions.js: This is going to be used for the JS logic needed for the site. Added some code for a button on the home page to help animate it.

--- static/js/functions.js
+++ static/js/functions.js
@@ -2,7 +2,6 @@
 
 
 
-
 // This is for the product page increase/decrease buttons.
 function increaseValue() {
   var value = parseInt(document.getElementById('productAmount').value, 10);
@@ -18,3 +17,24 @@ function decreaseValue() {
   value--;
   document.getElementById('productAmount').value = value;
 }
+
+
+// This is for the bubbly button animations
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+
