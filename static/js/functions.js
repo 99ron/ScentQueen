71cef58e1002672e49e4e5d5d779efce3b47fb90
Aaron <azza_5252@hotmail.com>
2021-01-25 19:00:29 +0000

    More javascript being added controlling the adding and removing off added items to cart. Display total items in cart above the cart icon on the navbar. Removed old logic for increasing item amount. Added additional JS on the base.html page to check total amount of items and to allow us to use AJAX to POST to our database.

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,24 +1,5 @@
 // This JS file is for functions needed to make certain elements work as intended.
 
-
-
-// This is for the product page increase/decrease buttons.
-function increaseValue() {
-  var value = parseInt(document.getElementById('productAmount').value, 10);
-  value = isNaN(value) ? 2 : value;
-  value++;
-  document.getElementById('productAmount').value = value;
-}
-
-function decreaseValue() {
-  var value = parseInt(document.getElementById('productAmount').value, 10);
-  value = isNaN(value) ? 2 : value;
-  value < 2 ? value = 2 : '';
-  value--;
-  document.getElementById('productAmount').value = value;
-}
-
-
 // This is for the bubbly button animations
 var animateButton = function(e) {
 
--- store/views.py
+++ store/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 from .models import *
 
 # Views for the store, cart and checkout.
 
 def store(request):
+    
+    if request.user.is_authenticated:
+        customer = request.user.id
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        # Create Empty cart for non logged in users (Temp)
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems = order['get_cart_items']
+        
+       
+        
     products = Product.objects.all()
-    context = { 'products':products}
+    context = { 'products':products, 'cartItems':cartItems}
     return render(request, 'store.html', context)
 
 
@@ -16,11 +32,12 @@ def cart(request):
         customer = request.user.id
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         # Create Empty cart for non logged in users (Temp)
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
-    context = {'items':items, 'order':order}
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'cart.html', context)
     
     
@@ -30,9 +47,38 @@ def checkout(request):
         customer = request.user.id
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         # Create Empty cart for non logged in users (Temp)
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
-    context = {'items':items, 'order':order}
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'checkout.html', context)
+    
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    
+    print('Action: ', action)
+    print('productId: ', productId)
+    
+    customer = request.user.id
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+    
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+            orderItem.quantity = (orderItem.quantity -1)
+    
+    orderItem.save()
+    
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+    
+    return JsonResponse('Item was added', safe=False)
