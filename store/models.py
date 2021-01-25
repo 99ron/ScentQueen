71d91ffa6159926ec8826515532b14bc80feaf5b
Aaron <azza_5252@hotmail.com>
2021-01-25 13:19:47 +0000

    store/models.py: Added more @properties to get total items and total costs for the cart. Updated both html files to use data passed through to the template oppose to hard coded text. views: This has added logic for both the cart and checkout page which is being fed data that's in the cart.

--- store/models.py
+++ store/models.py
@@ -20,6 +20,8 @@ class Product(models.Model):
             url = ''
         return url
 
+
+
 class Order(models.Model):
     customer = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
@@ -27,7 +29,21 @@ class Order(models.Model):
     transaction_id = models.CharField(max_length=100, null=True)
     
     def __str__(self):
-        return self.id
+        return str(self.id)
+        
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+  
+  
     
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
@@ -35,8 +51,12 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
     
-    def __str__(self):
-        return self.product.name
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+    
+    
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True)
@@ -49,4 +69,5 @@ class ShippingAddress(models.Model):
     def __str__(self):
         return self.address
         
-        
+
+
--- store/views.py
+++ store/views.py
@@ -8,10 +8,31 @@ def store(request):
     context = { 'products':products}
     return render(request, 'store.html', context)
 
+
+
 def cart(request):
-    context = {}
+    
+    if request.user.is_authenticated:
+        customer = request.user.id
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create Empty cart for non logged in users (Temp)
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items':items, 'order':order}
     return render(request, 'cart.html', context)
     
+    
+    
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.id
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create Empty cart for non logged in users (Temp)
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items':items, 'order':order}
     return render(request, 'checkout.html', context)
