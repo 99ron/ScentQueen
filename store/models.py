68f46b633024e0f27e141b5f43d0409f3663b27e
Aaron <azza_5252@hotmail.com>
2021-01-25 09:41:44 +0000

    Added the: Product, Order, OrderItem and Shipping models in the store app.

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from userProfile.models import UserProfile
 
 # models for the products
 
@@ -10,3 +11,34 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+    
+    def __str__(self):
+        return self.id
+    
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.product.name
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    county = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.address
+        
+        
