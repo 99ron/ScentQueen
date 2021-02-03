8b8ee8434805d6f9246c5f02c90088220080b499
Aaron <azza_5252@hotmail.com>
2021-02-03 00:30:05 +0000

    Added a new template page to view previous orders under the Profile tab. git add store/templates/checkout.html

--- userProfile/views.py
+++ userProfile/views.py
@@ -21,7 +21,7 @@ def user_profile(request):
             user = UserProfile.objects.get(pk=request.user.id)
            
         except Exception as e:
-            messages.error(request, "No profile was found, please contact the company")
+            messages.error(request, "No profile was found, please contact the company.")
             return render(request, 'home.html')       
         
         if request.method == 'GET':
@@ -30,9 +30,12 @@ def user_profile(request):
             formUser = userProfileForm(instance=user)
             
             # This matches the logged in user to the address table which then renders the form with the correct data.
-            address = UserAddress.objects.get(person=user)
-            formAddress = userAddressForm(instance=address)
-            
+            try:
+                address = UserAddress.objects.get(person=user)
+                formAddress = userAddressForm(instance=address)
+            except:
+                formAddress = userAddressForm()   
+                
         else:
             # Requests the form and files data.
             formUser = userProfileForm(request.POST)
@@ -41,15 +44,15 @@ def user_profile(request):
             if formUser.is_valid() and formAddress.is_valid():
                 
                 # Sets the unique user profile instance up. 
-                address = UserAddress.objects.get(person=user)
+                # address = UserAddress.objects.get(person=user)
                 up = user
-                ua = address
-                
+                ua = formAddress
                 try:
                     up.first_name = formUser.cleaned_data['first_name']
                     up.last_name = formUser.cleaned_data['last_name']
                     up.phone_number = formUser.cleaned_data['phone_number']
                     up.gender = formUser.cleaned_data['gender']
+                    up.email = formUser.cleaned_data['email']
                     
                     ua.town_city = formAddress.cleaned_data['town_city']
                     ua.street_address1 = formAddress.cleaned_data['street_address1']
@@ -59,6 +62,7 @@ def user_profile(request):
   
                     up.save()
                     ua.save()
+                    
                     messages.success(request, "Your profile was updated successfully!")
                     return redirect(user_profile)
                     
@@ -72,3 +76,11 @@ def user_profile(request):
     # If the user isn't logged in then redirect them to the log in page.
     else:
         return render(request, 'login.html')
+        
+
+# Will pull through orders when the database is set up for it. Now it'll simply render a page.
+@login_required
+def my_orders(request):
+    
+    
+    return render(request, 'my-orders.html')
