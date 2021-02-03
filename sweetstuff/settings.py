888679ac630782725c0ccabb578d116607fd9877
Aaron <azza_5252@hotmail.com>
2021-02-03 19:53:25 +0000

    Uploading additional files for it to be hosted on Heroku, probably forgotten something...

--- sweetstuff/settings.py
+++ sweetstuff/settings.py
@@ -138,6 +138,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+AWS_DEFAULT_ACL = 'public-read'
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires' : 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl' : 'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = 'scentqueen'
+AWS_S3_REGION_NAME = "eu-west-2"
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# s3 public media settings
+PUBLIC_MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+DEFAULT_FILE_STORAGE = 'scentqueen.storage_backends.PublicMediaStorage'
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
@@ -146,7 +168,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIAFILES_LOCATION = 'media'
-# DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
