f3fd4c4337901e80c1684103becf1d87785677e0
Aaron <azza_5252@hotmail.com>
2021-02-03 21:01:28 +0000

    Disabled 'import env' on the settings app.

--- sweetstuff/settings.py
+++ sweetstuff/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-import env
+# import env
 import dj_database_url 
 from pathlib import Path
 from django.contrib.messages import constants as messages
