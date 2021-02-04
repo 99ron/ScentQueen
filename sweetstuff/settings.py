b4e1fbd416a69048df1ddfd3d70fb1510b50f156
Aaron <azza_5252@hotmail.com>
2021-02-04 19:10:08 +0000

    Updated ALLOWED_HOSTS as I put in the www. and trailing /

--- sweetstuff/settings.py
+++ sweetstuff/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0825d3fb6bc043ce9f3ab28e4e45c6a8.vfs.cloud9.eu-west-2.amazonaws.com','https://scentqueen.herokuapp.com/']
+ALLOWED_HOSTS = ['0825d3fb6bc043ce9f3ab28e4e45c6a8.vfs.cloud9.eu-west-2.amazonaws.com','scentqueen.herokuapp.com']
 
 
 # Application definition
